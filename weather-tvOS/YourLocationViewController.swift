9b3a0273e97ce61c7faf9431d0e73b66dc151de1
merteyup <eupmrt@gmail.com>
2024-02-18 14:36:27 +0300

    Import `AVFoundation` and `initVideoBackground` with simple setup

--- weather-tvOS/YourLocationViewController.swift
+++ weather-tvOS/YourLocationViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class YourLocationViewController: UIViewController {
     
@@ -17,11 +18,28 @@ class YourLocationViewController: UIViewController {
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var nextDayButton: UIButton!
     
+    var avPlayer: AVPlayer!
+    var avPlayerLayer: AVPlayerLayer!
+    
     // MARK: - Statements
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // MARK: - Functions
+    func initVideoBackground() {
+        avPlayer = AVPlayer(playerItem: nil)
+        avPlayerLayer = AVPlayerLayer(player: avPlayer)
+        avPlayerLayer.videoGravity = .resizeAspectFill
+        avPlayerLayer.frame = view.layer.bounds
+        
+        avPlayer.volume = 0
+        avPlayer.actionAtItemEnd = .none
+        
+        view.backgroundColor = .clear
+        view.layer.insertSublayer(avPlayerLayer, at: 0)
+    }
+  
     // MARK: - Actions
     @IBAction func nextDayAction(_ sender: UIButton) {
         
